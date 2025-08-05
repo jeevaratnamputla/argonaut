3f31294dcb980071a228128049c2900b6f54b518
yugandhar <yugaa22@gmail.com>
2025-08-05 10:57:15 +0530

    clenup pythons

--- apps/call-similarity.py
+++
@@ -1,7 +0,0 @@
-from similarity import semantic_similarity
-
-text1 = "Why is application APP having issues"
-text2 = "Is application APP having issues"
-
-score = semantic_similarity(text1, text2)
-print(f"Semantic similarity score: {score}")
--- apps/replace-pnouns.py
+++
@@ -1,5 +0,0 @@
-from propernoun import replace_proper_nouns
-
-text = 'The repository "for-testing" is available but branch "main" is too. The path "/app/dest" is missing'
-modified = replace_proper_nouns(text)
-print("Modified Output:", modified)
